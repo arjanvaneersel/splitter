fe738654be7f39e450a62af82d9bdc61883d7768
Arjan van Eersel <arjan@vaneersel.eu>
2019-07-06 13:11:50 +0300

    Implemented modifiers and split function

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -7,6 +7,24 @@ contract Splitter {
     address carol;
     mapping(address => uint) balances;
 
+    // onlyOwner is a modifier to limit access to the owner
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    // onlyAlice is a modifier to limit access to Alice
+    modifier onlyAlice() {
+        require(msg.sender == alice);
+        _;
+    }
+
+    // onlyParticipant is a modifier to limit access to Alice, Bob or Carol
+    modifier onlyParticipant() {
+        require(msg.sender == alice || msg.sender == bob || msg.sender == carol);
+        _;
+    }
+
     // Constructor. Requires the addresses of alice, bob and carol
     constructor() public {
         owner = msg.sender;
@@ -31,5 +49,25 @@ contract Splitter {
     // getBalances returns the balances of Alice, Bob and Carol.
     function getBalances() public view returns (uint, uint, uint) {
         return (balances[alice], balances[bob], balances[carol]);
+    }
+
+    // topUp adds the paid value to the sender's balance.
+    function topUp() public payable onlyParticipant {
+        balances[msg.sender] += msg.value;
+    }
+
+    // split will split the paid amount over the balances of
+    // Bob and Carol. Any remainder will be added to the balance
+    // of the sender. This function can only be executed by Alice.
+    function split() public payable onlyAlice {
+        uint amount = msg.value;
+        uint half = amount / 2;
+        uint remainder = amount - (half * 2);
+
+        balances[bob] += half;
+        balances[carol] += half;
+        if (remainder > 0) {
+            balances[msg.sender] += remainder;
+        }
     }
 }
--- test/splitter.js
+++ test/splitter.js
@@ -16,4 +16,37 @@ contract('Splitter', (accounts) => {
         assert.equal(balances[1].toNumber(), 0, "bob's balance should be 0");
         assert.equal(balances[2].toNumber(), 0, "carol's balance should be 0");
     });
+
+    it('should top-up balances', async () => {
+        const splitterInstance = await Splitter.deployed();
+        await splitterInstance.topUp({value: 2, from: accounts[1]});
+
+        const balances = await splitterInstance.getBalances.call();
+        assert.equal(balances[0].toNumber(), 2, "alice's balance should be 2");
+        assert.equal(balances[1].toNumber(), 0, "bob's balance should be 0");
+        assert.equal(balances[2].toNumber(), 0, "carol's balance should be 0");
+    });
+
+    it('should split alice\'s payments', async () => {
+        const splitterInstance = await Splitter.deployed();
+        await splitterInstance.split({value: 25, from: accounts[1]});
+
+        const balances = await splitterInstance.getBalances.call();
+        assert.equal(balances[0].toNumber(), 3, "alice's balance should be 3");
+        assert.equal(balances[1].toNumber(), 12, "bob's balance should be 0");
+        assert.equal(balances[2].toNumber(), 12, "carol's balance should be 0");
+    });
+
+    it('should fail to split payment not from Alice', async () => {
+        const splitterInstance = await Splitter.deployed();
+
+        let error;
+        try {
+            await splitterInstance.split({value: 25, from: accounts[2]});
+        } catch(e) {
+            error = e;
+        }
+
+        assert.equal(error, 'Error: Returned error: VM Exception while processing transaction: revert');
+    });
 });
